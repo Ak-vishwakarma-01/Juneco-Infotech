39a289896f070adbeb4e7842f54d4e3a4e69764d
Ankit kumar Vishwakarma <100838272+Ak-vishwakarma-01@users.noreply.github.com>
2025-04-19 19:22:29 +0530

    day10 creatgin app with bootstrap

--- mineOne/9Calculator Python/calculator.py
+++ mineOne/9Calculator Python/calculator.py
@@ -1,26 +1,30 @@
 
 chose = True
 
-def check(a,b,type):
-    if(type==1): 
+def check(a,b,n):
+    if(n==1): 
         print( a+b)
-    elif(type==2):
+    elif(n==2):
         print(a-b)
-    elif(type==3): 
+    elif(n==3): 
         print(a*b)
     else: 
         print(a/b)
     return 
 
 while(chose):
-    type = int(input("enter 1 for addition   \n" 
+    n = int(input("enter 1 for addition   \n" 
     "2 for substraction \n"
     "3 for multipication \n" 
     "4 for devision \n"
     "or 5 to end this cycle: "))
-    a = int(input("enter the first number: "))
-    b = int(input("enter the second number: "))
-    check(a,b,type)
+    if n==5:
+        print("end of cycle")
+        break
+    else:
+        a = int(input("enter the first number: "))
+        b = int(input("enter the second number: "))
+        check(a,b,n)
     
     
 
